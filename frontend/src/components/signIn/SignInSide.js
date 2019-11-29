0b3a523d35ff8dc7277cfe1db606cd27f84e2fb0
Marek <marekllorinc@gmail.com>
2019-11-29 14:00:41 +0100

    added link to registration

--- frontend/src/components/signIn/SignInSide.js
+++ frontend/src/components/signIn/SignInSide.js
@@ -110,18 +110,12 @@ export default function SignInSide() {
             >
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
+            
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
-            </Grid>
             <Box mt={5}>
               <Copyright />
             </Box>
